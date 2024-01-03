3103d32c5c2a2136ea9d6021afb59f6a747d220b
Hema Sirisha <85925736+hemasirisha18@users.noreply.github.com>
2024-01-03 13:08:11 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request
+import pickle 
+import numpy as np
 # create a Flask object
 application = Flask(__name__)
 '''@app.route('/')
